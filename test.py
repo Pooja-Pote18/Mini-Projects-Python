68997de285bad99fa58811e762be9d2a59110125
Pooja-Pote18 <poojapote2003@gmail.com>
2025-03-29 16:10:13 +0530

    Update Digital Clock .py

--- test.py
+++ test.py
@@ -1,8 +1,3 @@
-'''import tkinter 
-win = tkinter.Tk()
-win.mainloop()
-'''
-
 # tkinter library install
 from tkinter import*
 import time as t
@@ -20,6 +15,7 @@ def time():
 #print Current Date
 print("Todays Time:", t.strftime("%d-%m-%y , %H:%M:%S %p "))
 
+#label
 l=Label(dc,font=('Arial',55), bg="black", fg="yellow", text="d")
 
 l.pack(anchor='center')
@@ -27,31 +23,3 @@ l.pack(anchor='center')
 time()
 dc.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-from datetime import datetime,date,time
-c_datetime = datetime.now()
-print("current date and time:",c_datetime)
-
-today_date = date.today()
-print("today date:",today_date)
-
-c_time=datetime.now().time()
-print("c_time:",c_time)
-
-formatted_datetime = c_datetime.strftime("%Y-%M-%D  %H:%M:%S:")
-print("foramatted date and time :",formatted_datetime)
-'''
